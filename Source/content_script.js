5cb628b6499a84b62063026343e6cff535494b75
PsKramer <payton@apexhorizon.net>
2016-01-09 17:23:05 -0800

    Update content_script.js
    !st real commit, added all gif to jif functions

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,11 +31,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
+	v = v.replace(/\bgif\b/g, "jif");
+	v = v.replace(/\bGif\b/g, "Jif");
+	v = v.replace(/\bGIF\b/g, "JIF");
+
 	textNode.nodeValue = v;
 }
 
